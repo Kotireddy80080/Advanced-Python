a96879ff77c346548b646536d1d114e94fb47a84
sanivarapu koti reddy <kotireddy@gmail.com>
2025-01-24 18:36:26 +0530

    Polymorphism-DuckTyping

---
+++ Polymorphism-DuckTyping.py
@@ -0,0 +1,35 @@
+print("Example-1")
+class I_HUB_IT_SERVICES:
+    def Developer(self):
+        print("Developers: Are responsible for developing application logic")
+    def FrontEnd_Developer(self):
+        print("FrontEnd_Developer: are responsible for to develop UI components")
+    def Tester(self):
+        print("Testers: are responsible for to debug the application code")
+    def BDM(self):
+        print("BDM:are responsible for Brings the new projects")
+i1=I_HUB_IT_SERVICES()
+i1.Developer()
+print()
+i1.FrontEnd_Developer()
+print()
+i1.Tester()
+print()
+i1.BDM()
+print()
+
+print("Example-2")
+class Calculator_Class:
+    def Add(self,X1,X2):
+        print("Sum is:",X1+X2) 
+    def Division(self,Y1,Y2):
+        print("Divison is:",Y1/Y2)
+    def Products(self,Z1,Z2):
+        print("Product is:",Z1*Z2)
+c1=Calculator_Class()
+c1.Add(1001,1002)
+print()
+c1.Division(100,25)
+print()
+c1.Products(10,15)
+print()
