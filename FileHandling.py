6ddd4275dc84694452fab2d95d886cbebd6002a5
sanivarapu koti reddy <kotireddy@gmail.com>
2025-02-04 18:28:14 +0530

    file handling

---
+++ FileHandling.py
@@ -0,0 +1,81 @@
+print("Example-1")
+import time 
+f1=open("ABC.txt","r")
+print("=====File_Details=====")
+print("File_Name is:",f1.name)
+print("File_Mode is:",f1.mode)
+print("File is closed or not:",f1.closed)
+print("File is readable or not:",f1.readable())
+print("File is writable or not:",f1.writable())
+print()
+f1.close()
+print()
+time.sleep(2)
+print("End of an application")
+
+
+print("Example-2")
+import time 
+f1=open("ABC.txt")
+print("=====File_Details=====")
+print("File_Name is:",f1.name)
+print("File_Mode is:",f1.mode)
+print("File is closed or not:",f1.closed)
+print("File is readable or not:",f1.readable())
+print("File is writable or not:",f1.writable())
+print()
+f1.close()
+print()
+time.sleep(2)
+print("End of an application")
+
+
+print("Example-3")
+import time 
+f1=open("ABC.txt","r+")
+print("=====File_Details=====")
+print("File_Name is:",f1.name)
+print("File_Mode is:",f1.mode)
+print("File is closed or not:",f1.closed)
+print("File is readable or not:",f1.readable())
+print("File is writable or not:",f1.writable())
+print()
+f1.close()
+print()
+time.sleep(2)
+print("End of an application")
+
+
+print("Example-4")
+import time 
+f1=open("ABC.txt","w")
+print("=====File_Details=====")
+print("File_Name is:",f1.name)
+print("File_Mode is:",f1.mode)
+print("File is closed or not:",f1.closed)
+print("File is readable or not:",f1.readable())
+print("File is writable or not:",f1.writable())
+print()
+f1.close()
+print()
+time.sleep(2)
+print("End of an application")
+
+
+print("Example-5")
+import time 
+f1=open("ABC.txt","w+")
+print("=====File_Details=====")
+print("File_Name is:",f1.name)
+print("File_Mode is:",f1.mode)
+print("File is closed or not:",f1.closed)
+print("File is readable or not:",f1.readable())
+print("File is writable or not:",f1.writable())
+print()
+f1.close()
+print()
+time.sleep(2)
+print("End of an application")
+
+
+
